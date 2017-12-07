f2b465508866ce96c489749ca27b1d90fdd9a23f
DarrenLu <qoogood78@gmail.com>
2017-12-08 00:17:46 +0800

    04-23 Ignoring Files with Webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
 	// Inform webpack thay we're building a bundle
@@ -17,6 +18,7 @@ const config = {
 		filename: 'bundle.js',
 		// will automatically generate build folder
 		path: path.resolve(__dirname, 'build')
-	}
+	},
+	externals: [webpackNodeExternals()]
 }
 module.exports = merge(baseConfig, config)
