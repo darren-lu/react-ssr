516fe5806a33810dd47d208243eb1d119e023c17
DarrenLu <qoogood78@gmail.com>
2017-12-10 02:07:22 +0800

    07-45 The MatchRoutes Function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill'
 import express from 'express'
+import { matchRoutes } from 'react-router-config'
+import Routes from './client/Routes'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
 
@@ -8,8 +10,7 @@ const app = express()
 app.use(express.static('public'))
 app.get('*', (req, res) => {
 	const store = createStore()
-	// Some logic to initialize
-	// and load data into the store
+	console.log(matchRoutes(Routes, req.path))
 	res.send(renderer(req, store))
 })
 
