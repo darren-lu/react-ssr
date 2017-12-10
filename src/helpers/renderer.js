65c0c50df7721e7f6b971684ccaf98705691916b
DarrenLu <qoogood78@gmail.com>
2017-12-11 06:47:46 +0800

    08-56 Mitigating XSS Attacks

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,7 +3,7 @@ import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
-
+import serialize from 'serialize-javascript'
 import Routes from '../client/Routes'
 
 export default (req, store) => {
@@ -14,16 +14,17 @@ export default (req, store) => {
 			</StaticRouter>
 		</Provider>
 	)
+
 	return `
     <html>
       <head></head>
-    <body>
-      <div id="root">${content}</div>
-      <script>window.INITIAL_STATE = ${JSON.stringify(
-				store.getState()
-			)}</script>
-      <script src="bundle.js"></script>
-    </body>
+      <body>
+        <div id="root">${content}</div>
+        <script>
+          window.INITIAL_STATE = ${serialize(store.getState())}
+        </script>
+        <script src="bundle.js"></script>
+      </body>
     </html>
   `
 }
